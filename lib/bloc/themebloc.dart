cfd0d86a7b70e3d296baf02494bb1b630788e6dd
Mahdinaghikhani <mahdinkh7@gmail.com>
2022-05-24 17:32:17 +0430

    create BlocStaet

---
+++ lib/bloc/themebloc.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+@immutable
+abstract class BlocState {}
+
+class Initial extends BlocState {}
+
+class Loading extends BlocState {}
+
+class Failed extends BlocState {}
+
+class Authenticated extends BlocState {}
