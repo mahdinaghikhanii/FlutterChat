11c7288ea1edf47fe5fdf29ab826c4af9a495aa6
Mahdinaghikhani <mahdinkh7@gmail.com>
2022-05-31 09:34:27 +0430

    Create user_data amd starting develop user_Data

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutterchat/module/constant.dart';
+import 'package:flutterchat/module/extension.dart';
 
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: false,
+        title: Text(
+          'Online',
+          style: context.textTheme.subtitle2!.copyWith(
+              color: kwhite, fontWeight: FontWeight.bold, fontSize: 24),
+        ),
+      ),
+    );
   }
 }
